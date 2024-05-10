5809ef99379f4627633a5e71fef2f4692c0955f8
David Korczynski <david@adalogics.com>
2024-05-10 16:48:05 -0700

    cleanup
    Signed-off-by: David Korczynski <david@adalogics.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-for file in "minmea.c"; do
-  $CC $CFLAGS -c ${file}
-done
+$CC $CFLAGS -c minmea.c
 
-rm -f ./test*.o
 llvm-ar rcs libfuzz.a *.o
-
 
 $CC $CFLAGS $LIB_FUZZING_ENGINE $SRC/fuzzer.c -Wl,--whole-archive $SRC/minmea/libfuzz.a -Wl,--allow-multiple-definition -I$SRC/minmea/ -I$SRC/minmea/compat  -o $OUT/fuzzer
